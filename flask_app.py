4f504158383133718ad2ea8568fba702c58b80fa
Nikita Koba <nikitakoba93@gmail.com>
2020-01-02 17:19:24 -0500

    changes to css

--- flask_app.py
+++ flask_app.py
@@ -341,5 +341,5 @@ def not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    #app.run(debug=True, port=8000, host='0.0.0.0')
-    app.run()
+    app.run(debug=True, port=8000, host='0.0.0.0')
+    #app.run()
